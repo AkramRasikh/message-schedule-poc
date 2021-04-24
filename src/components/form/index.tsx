d2a464fcc6d5477b059b70a69e3677f567080954
akram rasikh <akram.rasikh@gmail.com>
2021-04-24 11:53:08 +0100

    error message showing for max but not min lenth

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,34 +1,43 @@
 import React from 'react';
-import { FormControl, Grid } from '@material-ui/core';
+import { Button, FormControl, Grid } from '@material-ui/core';
 import { useForm } from 'react-hook-form';
 import { FormFieldsProps } from '../../form-fields/twitter-fields';
 
 const Form: React.FC<FormFieldsProps> = ({ fields }) => {
-  const { register, handleSubmit } = useForm({ mode: 'onChange' });
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
   const onSubmit = (fieldData: any) => {
     console.log('fieldData: ', fieldData);
   };
 
   return (
-    <FormControl onSubmit={handleSubmit(onSubmit)}>
-      {fields.map(
-        ({ InputComponent, name, label, required, rules, ...fieldProps }) => (
-          <Grid key={name}>
-            <InputComponent
-              name={name}
-              label={label}
-              {...fieldProps}
-              register={register(name, {
-                required,
-                ...rules,
-              })}
-            />
-          </Grid>
-        ),
-      )}
-      <button>Submit</button>
-    </FormControl>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl>
+        {fields.map(
+          ({ InputComponent, name, label, required, rules, ...fieldProps }) => {
+            return (
+              <Grid key={name}>
+                <InputComponent
+                  name={name}
+                  label={label}
+                  register={register(name, {
+                    required,
+                    ...rules,
+                  })}
+                  {...fieldProps}
+                />
+                {errors[name] && <span>{errors[name].message}</span>}
+              </Grid>
+            );
+          },
+        )}
+        <Button type='submit'>Submit</Button>
+      </FormControl>
+    </form>
   );
 };
 
--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
-import { Button } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
 
 function rand() {
   return Math.round(Math.random() * 20) - 10;
@@ -22,7 +22,7 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     paper: {
       position: 'absolute',
-      width: 400,
+      width: '80%',
       backgroundColor: theme.palette.background.paper,
       border: '2px solid #000',
       boxShadow: theme.shadows[5],
@@ -60,7 +60,7 @@ const SimpleModal: React.FC<SimpleModalProps> = ({
   );
 
   return (
-    <div>
+    <Grid>
       <Button type='button' onClick={handleOpen}>
         {openModalText}
       </Button>
@@ -72,7 +72,7 @@ const SimpleModal: React.FC<SimpleModalProps> = ({
       >
         {body}
       </Modal>
-    </div>
+    </Grid>
   );
 };
 
